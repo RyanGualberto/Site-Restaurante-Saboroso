3ce6c8b185e78abc3307dea86990c93389c3af60
Ryan Gualberto <ryanoliveiragualberto@gmail.com>
2022-07-08 10:56:27 -0300

    aula 179: refactoring menu

--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,35 @@
 var express = require("express");
+var admin = require("../inc/admin");
 var router = express.Router();
 var users = require('./../inc/users')
 
 router.use(function (req, res, next) {
     if (['/login'].indexOf(req.url) === -1 && !req.session.user) {
         res.redirect('/admin/login');
-    }else{
+    } else {
         next();
     }
 });
 
-router.get("/logout", function(req, res, next) {
+router.use(function (req, res, next) {
+    req.menus = admin.getMenus();
+    next();
+});
+
+router.get("/logout", function (req, res, next) {
     delete req.session.user;
     res.redirect('/admin/login')
 });
 
 router.get('/', function (req, res, next) {
-
-    res.render('admin/index');
+    res.render(('admin/index'), {
+        menus: req.menus
+    });
 });
 router.get('/reservations', function (req, res, next) {
     res.render('admin/reservations', {
-        date: {}
+        date: {},
+        menus: req.menus
     });
 });
 router.get('/login', function (req, res, next) {
@@ -44,22 +52,22 @@ router.post('/login', function (req, res, next) {
 });
 router.get('/contacts', function (req, res, next) {
     res.render('admin/contacts', {
-
+        menus: req.menus
     });
 });
 router.get('/emails', function (req, res, next) {
     res.render('admin/emails', {
-
+        menus: req.menus
     });
 });
 router.get('/users', function (req, res, next) {
     res.render('admin/users', {
-
+        menus: req.menus
     });
 });
 router.get('/menus', function (req, res, next) {
     res.render('admin/menus', {
-
+        menus: req.menus
     });
 });
 
