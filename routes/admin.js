e03a465d832a949f02c83280f29e98cb31a8c06b
Ryan Gualberto <ryanoliveiragualberto@gmail.com>
2022-07-08 09:45:43 -0300

    aula 175: config redis

--- routes/admin.js
+++ routes/admin.js
@@ -1,32 +1,34 @@
 var express = require("express");
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.render('admin/index', {
 
     });
 });
-router.get('/reservations', function(req, res, next) {
+router.get('/reservations', function (req, res, next) {
     res.render('admin/reservations', {
         date: {}
     });
 });
-router.get('/login', function(req, res, next) {
-    res.render('admin/login', {
+router.get('/login', function (req, res, next) {
+    if (!req.session.views) req.session.views = 0;
 
-    });
+    console.log('SESSION ',req.session.views++)
+
+    res.render('admin/login');
 });
-router.get('/contacts', function(req, res, next) {
+router.get('/contacts', function (req, res, next) {
     res.render('admin/contacts', {
 
     });
 });
-router.get('/emails', function(req, res, next) {
+router.get('/emails', function (req, res, next) {
     res.render('admin/emails', {
 
     });
 });
-router.get('/users', function(req, res, next) {
+router.get('/users', function (req, res, next) {
     res.render('admin/users', {
 
     });
