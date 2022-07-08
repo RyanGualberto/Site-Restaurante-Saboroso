d619bd962110e853e5783c7d7d8edaa3d187c1bb
Ryan Gualberto <ryanoliveiragualberto@gmail.com>
2022-07-08 08:42:04 -0300

    aula 172: send reservation to database

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var conn = require('./../inc/db');
 var express = require('express');
-var menus = require('./../inc/menus')
+var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservations')
 var router = express.Router();
 
 /* GET home page. */
@@ -15,7 +16,7 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/contacts', function (req, res, next) {
-  
+
   res.render('contacts', {
     title: 'Contato - Restaurante Saboroso!',
     background: 'images/img_bg_3.jpg',
@@ -43,10 +44,33 @@ router.get('/services', function (req, res, next) {
 });
 
 router.get('/reservations', function (req, res, next) {
+  reservations.render(req, res)
   res.render('reservations', {
     title: 'Reservas - Restaurante Saboroso!',
     background: 'images/img_bg_2.jpg',
     h1: 'Reserve uma Mesa!'
   });
+});
+
+router.post('/reservations', function (req, res, next) {
+  if (!req.body.name) {
+    reservations.render(req, res, 'digite um nome');
+  } else if (!req.body.email) {
+    reservations.render(req, res, 'digite um email');
+  } else if (!req.body.people) {
+    reservations.render(req, res, 'Selecione a Quantidade de Pessoas');
+  } else if (!req.body.date) {
+    reservations.render(req, res, 'escolha um data');
+  } else if (!req.body.time) {
+    reservations.render(req, res, 'selecione a hora');
+  } else {
+    reservations.save(req.body).then(results => {
+      req.body = {};
+      reservations.render(req, res, null, "reserva realizada com sucesso!");
+
+    }).catch(err => {
+      reservations.render(req, res, err.message);
+    });
+  }
 });
 module.exports = router;
