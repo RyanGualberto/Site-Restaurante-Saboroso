33b9d767d63fb45a125204c38320bca842a1a655
Ryan Gualberto <ryanoliveiragualberto@gmail.com>
2022-07-08 11:24:45 -0300

    aula 181: show data from mySql

--- inc/admin.js
+++ inc/admin.js
@@ -1,6 +1,25 @@
-const { param } = require("../routes/admin");
+var conn = require('./db');
 
 module.exports = {
+
+    dashboard(){
+        return new Promise((s, f) => {
+            conn.query(`
+            SELECT
+            (SELECT COUNT(*) FROM tb_contacts) AS nrcontacts,
+            (SELECT COUNT(*) FROM tb_menus) AS nrmenus,
+            (SELECT COUNT(*) FROM tb_reservations) AS nrreservations,
+            (SELECT COUNT(*) FROM tb_users) AS nrusers;
+            `, (err, results) => {
+                if (err) {
+                    f(err);
+                } else {
+                    s(results[0])
+                }
+            });
+        });
+    },
+
     getParams(req, params){
         return Object.assign({}, {
             menus: req.menus,
--- routes/admin.js
+++ routes/admin.js
@@ -22,7 +22,13 @@ router.get("/logout", function (req, res, next) {
 });
 
 router.get('/', function (req, res, next) {
-    res.render(('admin/index'), admin.getParams(req));
+    admin.dashboard().then(data => {
+        res.render(('admin/index'), admin.getParams(req, {
+            data
+        }));
+    }).catch(err => {
+        console.err(err);
+    })
 });
 router.get('/reservations', function (req, res, next) {
     res.render('admin/reservations', admin.getParams(req,{
